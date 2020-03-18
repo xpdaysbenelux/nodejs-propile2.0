5606bb4b24312e06051e191c21f549ef2d438b45
Agnes De Pestel <agnes.depestel@icapps.com>
2020-03-18 15:28:49 +0100

    added the Room & Event entity

---
+++ src/database/entities/event.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Program } from './program.entity';
+import { Room } from './room.entity';
+
+@Entity()
+export class Event extends BaseEntity {
+    @Column({ nullable: false })
+    title: string;
+
+    @Column({ default: false, nullable: false })
+    spanRow: boolean;
+
+    @Column({ nullable: false })
+    startTime: Date;
+
+    @Column({ nullable: false })
+    endTime: Date;
+
+    @ManyToOne(() => Program, { cascade: ['insert'] })
+    program: Program;
+
+    @ManyToOne(() => Room, { cascade: ['insert'] })
+    room: Room;
+}
---
+++ src/database/entities/room.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Conference } from './conference.entity';
+
+@Entity()
+export class Room extends BaseEntity {
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ nullable: false })
+    maxParticipants: number;
+
+    @Column({ nullable: true, type: 'text' })
+    location?: string;
+
+    @ManyToOne(() => Conference, { cascade: ['insert'] })
+    conference: Conference;
+}
