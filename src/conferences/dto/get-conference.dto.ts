f444f6381d917404fcc34c4d6e8b4337853ba5ae
Agnes De Pestel <agnes.depestel@icapps.com>
2020-03-19 14:12:36 +0100

    update get-conference dto

--- src/conferences/dto/get-conference.dto.ts
+++ src/conferences/dto/get-conference.dto.ts
@@ -1,7 +1,14 @@
-import { BaseEntityResponse } from 'src/_shared/dto';
+import { BaseEntityResponse } from '../../_shared/dto';
 
 export class ConferenceResponse extends BaseEntityResponse {
     readonly name: string;
     readonly startDate: Date;
     readonly endDate: Date;
+    readonly rooms: RoomResponse[];
+}
+
+class RoomResponse {
+    readonly id: string;
+    readonly name: string;
+    readonly maxParticipants: string;
 }
