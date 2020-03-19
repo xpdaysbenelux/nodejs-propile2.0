b047efd249a582012469b34df25700c6b13a0986
Agnes De Pestel <agnes.depestel@icapps.com>
2020-03-19 14:45:36 +0100

    finished createConference in controler

--- src/conferences/conferences.controller.ts
+++ src/conferences/conferences.controller.ts
@@ -1,19 +1,24 @@
 import { Controller, UseGuards, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+
 import { RequiredPermissions, UserSession } from '../_shared/decorators';
 import {
     RequiredPermissionsGuard,
     AuthenticatedGuard,
 } from '../_shared/guards';
 import { ConferencesService } from './conferences.service';
-import { CreateConferenceRequest } from './dto';
-import { IUserSession } from 'src/_shared/constants';
+import { CreateConferenceRequest, ConferenceResponse } from './dto';
+import { IUserSession } from '../_shared/constants';
+import { ConferencesQueries } from './conferences.queries';
 
 @UseGuards(AuthenticatedGuard)
 @Controller('conferences')
 @ApiTags('conferences')
 export class ConferencesController {
-    constructor(private readonly conferencesService: ConferencesService) {}
+    constructor(
+        private readonly conferencesService: ConferencesService,
+        private readonly conferenceQueries: ConferencesQueries,
+    ) {}
 
     @RequiredPermissions({ conferences: { edit: true } })
     @UseGuards(RequiredPermissionsGuard)
@@ -21,10 +26,11 @@ export class ConferencesController {
     async createConference(
         @Body() body: CreateConferenceRequest,
         @UserSession() session: IUserSession,
-    ): Promise<void> {
+    ): Promise<ConferenceResponse> {
         const conferenceId = await this.conferencesService.createConference(
             body,
             session,
         );
+        return this.conferenceQueries.getConference(conferenceId);
     }
 }
